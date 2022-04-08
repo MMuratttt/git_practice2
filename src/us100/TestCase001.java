89801cf47c1beaea6f2d8c5dce8c9f169aa22ea3
MMuratttt <muratulusal@yahoo.com>
2022-04-08 14:11:33 +0300

    First Commit

---
+++ src/us100/TestCase001.java
@@ -0,0 +1,11 @@
+package us100;
+
+public class TestCase001 {
+
+    // Hello
+    public static void main(String[] args) {
+        System.out.println("Wooden Spoon");
+
+    }
+
+}
---
+++ src/us100/TestCase002.java
@@ -0,0 +1,15 @@
+package us100;
+
+public class TestCase002 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        System.out.println("How are you");
+
+        System.out.println("Everything is Okey?");
+
+
+    }
+}
